bf165d4b212a59e19039fb1839aa93276e13b27e
Binh.BuiDang <dangbinh4869@gmail.com>
2025-12-02 21:54:11 +0700

    remove.

--- include/ui/sqlite_hierarchy.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <memory>
-
-class SQLiteDatabase;
-
-namespace SQLiteHierarchy {
-    void renderSQLiteHierarchy(const std::shared_ptr<SQLiteDatabase>& sqliteDb);
-} // namespace SQLiteHierarchy
